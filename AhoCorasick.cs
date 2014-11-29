06b97ded80808fafe3cb6111c10105099d50fa06
tackman <presitac@gmail.com>
2014-11-28 23:42:45 -0800

    add main source files

---
+++ AhoCorasick.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Collections.Generic;
+
+namespace Bouningen.Lib
+{
+    // Aho-Corasick法を使って検索・置換を行う
+    // 制限事項：Tはintをインデックスにしてアクセス出来るArrayを作れること
+    static class AhoCorasick<T>
+    {
+        public static void convert(TrieTree<T> tree, T[] buf)
+        {
+            System.Diagnostics.Debug.Assert(tree.root.failure == null);
+            for (int i = 0; i < buf.Length; ++i)
+            {
+                foreach (TrieNode<T> t in tree.root.children)
+                {
+                    if (t.obj.Equals(buf[i]))
+                    {
+                        i = search(t, buf, i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static int recusiveDepth = 0;
+
+        public static int search(TrieNode<T> node,  T[] buf,int cur)
+        {
+
+
+            ++recusiveDepth;
+            const bool _ = true;
+            for (; _; )
+            {
+            LOOPHEAD:
+                // 呼び出し時不変条件：node.obj eq buf[cur]
+                // 再帰デバッグ用temporary仕様 node == rootの場合もあり
+#if false
+            System.Diagnostics.Debug.Assert(node.failure != null);
+#endif
+
+                if (node.children.Count == 0)
+                {
+                    if (node.substitue != null)
+                    {
+                        replace(node.substitue, buf, cur);
+                    }
+                    return cur;
+                }
+
+                if (cur + 1 >= buf.Length)
+                {
+                    return cur;
+                }
+
+                foreach (TrieNode<T> t in node.children)
+                {
+                    if (t.obj.Equals(buf[cur + 1]))
+                    {
+                     //   return search(t, buf, cur + 1);
+                        cur++;
+                        node = t;
+                        goto LOOPHEAD;
+                    }
+                }
+
+#if false
+            if (node.failure.failure != null )
+            {
+                System.Diagnostics.Debug.Assert(node.obj.Equals( node.failure.obj));
+                return search(node.failure, buf, cur);
+            }
+
+            return cur;
+#else
+                if (node.failure == null)
+                {
+                    break ;
+                }
+
+                node = node.failure;
+
+            }
+
+            return cur;
+#endif
+        }
+
+        public static void replace(Stack<T> rep, T[] buf, int cur)
+        {
+            while( rep.Count > 0)
+            {
+                buf[cur] = rep.Pop();
+                --cur;
+            }
+        }
+    }
+}
---
+++ TrieTree.cs
@@ -0,0 +1,102 @@
+﻿
+using System.Collections.Generic;
+
+namespace Bouningen.Lib
+{
+    class TrieNode<T>
+    {
+        public TrieNode()
+        {
+            children = new Stack<TrieNode<T>>();
+            substitue = null;
+            failure = null;
+        }
+        public Stack<TrieNode<T>> children;
+        public T obj;
+        public TrieNode<T> failure;
+        public Stack<T> substitue; // 置換後パターン
+    }
+
+    class TrieTree<T>
+    {
+        public TrieTree()
+        {
+            root = new TrieNode<T>();
+            cur = root;
+        }
+
+        public TrieNode<T> root;
+        private TrieNode<T> cur;
+
+        public void add(ref T x, TrieNode<T> node)
+        {
+            cur = node;
+            add(ref x);
+        }
+
+        public void setRoot()
+        {
+            cur = root;
+        }
+
+
+        public void add(ref T x)
+        {
+            TrieNode<T> tmp = new TrieNode<T>();
+            tmp.obj = x;
+
+            foreach (TrieNode<T> t in cur.children)
+            {
+                if (t.obj.Equals( x ))
+                {
+                    cur = t;
+                    return;
+                }
+            }
+
+            cur.children.Push(tmp);
+            cur = cur.children.Peek();
+            return;
+
+        }
+
+
+        public void makeFailure()
+        {
+            foreach (TrieNode<T> t in root.children)
+            {
+                makeFailure(t, root);
+            }
+        }
+        public void makeFailure(TrieNode<T> now, TrieNode<T> goTo)
+        {
+            now.failure = goTo;
+            foreach (TrieNode<T> t in now.children)
+            {
+                foreach (TrieNode<T> u in goTo.children)
+                {
+                    if (t.obj.Equals(u.obj))
+                    {
+                        makeFailure(t, u);
+                        break;
+                    }
+                }
+
+                if (t.failure == null)
+                {
+                    makeFailure(t, root);
+                }
+            }
+        }
+
+        public void addSubstitue(T t)
+        {
+            if (null == cur.substitue)
+            {
+                cur.substitue = new Stack<T>();
+            }
+            cur.substitue.Push(t);
+        }
+
+    }
+}
